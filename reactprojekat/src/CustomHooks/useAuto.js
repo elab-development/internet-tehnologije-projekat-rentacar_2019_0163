54557ae27ecc675d118cc51a38040f3c7b6ebe83
sebastijanv <vlatkovicsebastijan@gmail.com>
2024-04-13 22:33:17 +0200

    kreirana custom hook-a

---
+++ reactprojekat/src/CustomHooks/useAuto.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useAuto = () => {
+  const [autos, setAutos] = useState([]);
+  
+  useEffect(() => {
+    // Funkcija za dobijanje tokena iz session storage-a
+    const getTokenFromStorage = () => {
+      const token = sessionStorage.getItem('token');
+      return token;
+    };
+    
+    // Axios GET zahtev ka serveru
+    const fetchAutos = async () => {
+      try {
+        const token = getTokenFromStorage();
+        const response = await axios.get('http://127.0.0.1:8000/api/auto', {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        // Postavljanje dobijenih podataka u state
+        setAutos(response.data);
+      } catch (error) {
+        console.error('Error fetching autos:', error);
+        // Možete dodati i neki vid obrade greške ovde
+      }
+    };
+
+    fetchAutos(); // Pozivanje funkcije za dobijanje podataka pri svakom mount-u komponente
+
+  }, []); // Prvi argument useEffect-a je prazan niz kako bi se ova funkcija pozvala samo jednom, kao componentDidMount
+
+  return [autos, setAutos]; // Vraćanje niza i setera za niz
+};
+
+export default useAuto;
