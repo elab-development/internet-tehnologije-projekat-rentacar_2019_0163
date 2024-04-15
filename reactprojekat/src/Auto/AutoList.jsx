fbe95eb3b2e5138f25b974da089385fd1001bda0
sebastijanv <vlatkovicsebastijan@gmail.com>
2024-04-15 17:59:06 +0200

    dodata paginacija

--- reactprojekat/src/Auto/AutoList.jsx
+++ reactprojekat/src/Auto/AutoList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import useAuto from '../CustomHooks/useAuto';
 import AutoCard from './AutoCard';
 import './AutoCard.css';
-
+import { MdNavigateNext,MdNavigateBefore } from "react-icons/md";
 const AutoList = () => {
   const [autos, setAutos] = useAuto();
   const [filters, setFilters] = useState({
@@ -12,10 +12,13 @@ const AutoList = () => {
     cena_po_danu: '',
     broj_vrata: '',
   });
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6; // Broj automobila po stranici
 
   const handleFilterChange = (e) => {
     const { name, value } = e.target;
     setFilters(prev => ({ ...prev, [name]: value }));
+    setCurrentPage(1); // Resetujemo trenutnu stranicu na prvu kada se primeni novi filter
   };
 
   const filteredAutos = autos.filter(auto => (
@@ -25,7 +28,14 @@ const AutoList = () => {
     (filters.cena_po_danu ? auto.cena_po_danu <= Number(filters.cena_po_danu) : true) &&
     (filters.broj_vrata ? auto.broj_vrata === Number(filters.broj_vrata) : true)
   ));
-  
+
+  // Izračunavanje indeksa prvog i poslednjeg automobila na trenutnoj stranici
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentAutos = filteredAutos.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Funkcija za promenu trenutne stranice
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <>
@@ -53,12 +63,20 @@ const AutoList = () => {
             <label htmlFor="broj_vrata">Broj vrata:</label>
             <input type="number" id="broj_vrata" name="broj_vrata" value={filters.broj_vrata} onChange={handleFilterChange} />
           </div>
+          <div className="pagination">
+          <button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}><MdNavigateBefore /></button>
+          <span>Trenutna stranica: {currentPage}</span>
+          <button onClick={() => paginate(currentPage + 1)} disabled={currentAutos.length < itemsPerPage}><MdNavigateNext /></button>
         </div>
+        </div>
+
         <div className="auto-card-container">
-          {filteredAutos.map(auto => (
+          {currentAutos.map(auto => (
             <AutoCard key={auto.id} auto={auto} />
           ))}
         </div>
+       
+
       </div>
     </>
   );
